788229e670ebec7c96044fcb37e7c3b59335f5a7
hmtcelik <abhmtcelik@gmail.com>
2022-04-21 19:13:05 +0300

    init

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="grid-container">
+                <div className="left">test</div>
+                <div className="middle">test</div>
+                <div className="right">test</div>
+            </div>
+        </div>
+    );
+}
+ 
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="links">
+            <h2>Area-Volume Calculator</h2>
+            </div>
+        </div>    
+    );
+}
+ 
+export default Navbar
